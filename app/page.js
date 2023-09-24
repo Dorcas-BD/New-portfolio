ba023d643dddc1289b9d5b79ae50d2cdadee134a
Dorcas Bamisile <bamisiledorcas@gmail.com>
2023-09-24 18:10:53 +0100

    Page

--- app/page.js
+++ app/page.js
@@ -1,9 +1,20 @@
 import Image from "next/image";
+import NavBar from "./components/NavBar/NavBar";
+import Hero from "./components/Hero/Hero";
+import About from "./components/About/About";
+import Projects from "./components/Projects/Projects";
+import Contacts from "./components/Contacts/Contacts";
+import Footer from "./components/Footer/Footer";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Initial Commit
-    </main>
+    <div className="main-container">
+      <NavBar />
+      <Hero />
+      <About />
+      <Projects />
+      <Contacts />
+      <Footer />
+    </div>
   );
 }
